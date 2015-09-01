f3b21a96cc90e898d658e8269c37df8ad94c5111
fuy219 <fuy219@lehigh.edu>
2015-09-01 15:05:21 +0000

    updatedUpdated WelcomeClass.java

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+//Fung Yeung!!
+//hw01
+//Print screen
+//Compile the file
+//Run the file
+
+public class WelcomeClass{
+    
+    public static void main(String[]args) {
+            
+        System.out.println("  -----------");
+        System.out.println("  | Welcome |");
+        System.out.println("  -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+        System.out.println("|-F--U--Y--2--1--9|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        System.out.println("Today I played ultimate frisbee.");
+        System.out.println("It was harder than expected.");
+        }
+}
