5d6ca8ef6b1646fb7043c6373f00472b7448ba8a
fuy219 <fuy219@lehigh.edu>
2015-10-28 00:54:08 +0000

    update lab09

--- lab09/coinflip.java
+++ lab09/coinflip.java
@@ -1,39 +1,38 @@
-import java.util.Scanner;
-public class coinflip{
-	public static int flip(){
-		int output=0;
-		int flip= (int) (Math.random()+.5);
-		return flip;
+import java.util.Scanner;// import scanner
+public class coinflip{// declare class
+	public static int flip(){//add flip method to flip coin
+		int flip= (int) (Math.random()+.5);//random generation to flip coin
+		return flip;// return the flip value
 		
 	}
-public static void flip(int a){
-	int heads=0;
-	int tails=0;
-	for(int i=0;i<a;i++){
-		int flip= flip();
+public static void flip(int a){// add flip method (overload) use to flip x number of times
+	int heads=0;// count for heads flipped
+	int tails=0;// count for tails flipped
+	for(int i=0;i<a;i++){//for loop to go through number of flips
+		int flip= flip();// calling first flip method
 			
-		if (flip==0){
-			System.out.println("Heads 0");
-			heads++;
+		if (flip==0){// if statement for heads flip
+			System.out.println("Heads 0");// print out the head flip
+			heads++;// count number of times heads was flipped
 		}
-		if (flip==1){
-			System.out.println("Tail 1");
-			tails++;
+		if (flip==1){// if statement for tails flip
+			System.out.println("Tail 1");// print out the tail flip
+			tails++;// count number of times tails was flipped
 		}
 	}
-	System.out.println("Number of heads "+ heads);
-	System.out.println("Number of tails "+ tails);
+	System.out.println("Number of heads "+ heads);// print out tails flip
+	System.out.println("Number of tails "+ tails);// print out heads flip
 	
 }
-public static void main(String[]args){
-	Scanner myScanner= new Scanner(System.in);
-	System.out.print("Enter number of flips: ");
-	int numberFlips= myScanner.nextInt();
-	if (numberFlips>=0 && numberFlips<=100){
+public static void main(String[]args){// add main method
+	Scanner myScanner= new Scanner(System.in);//initialize scanner
+	System.out.print("Enter number of flips: ");// ask user imput for number of flips
+	int numberFlips= myScanner.nextInt();// store number of flips
+	if (numberFlips>=0 && numberFlips<=100){// if statement for user input of 0-100 to do coin flip
 		flip(numberFlips);
 	}
-	else{
-		System.exit(-1);
+	else{// if statement for user input of invalid number
+		System.exit(-1);//exits program
 		
 }
 }
